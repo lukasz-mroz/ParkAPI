38a2eaa13d6aeeb0b47f9ec941f035632d03d27b
Lukasz Mroz <mrozikpl@gmail.com>
2021-02-28 13:41:19 +0100

    Creating a model of a Park

--- Park.Core/Models.cs
+++ Park.Core/Models.cs
@@ -1,9 +1,17 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Park.Core
 {
   public class Models
   {
-
+    [Key]
+    public int Id { get; set; }
+    [Required]
+    public string Name { get; set; }
+    [Required]
+    public string State { get; set; }
+    public DateTime Created { get; set; }
+    public DateTime Established { get; set; }
   }
 }
--- Park.Infrastructure/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace Park.Infrastructure
-{
-  public class Class1
-  {
-  }
-}
