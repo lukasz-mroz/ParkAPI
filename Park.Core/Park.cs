4986e0fe2a99864c3e70ef3a01c36b407626622f
Lukasz Mroz <mrozikpl@gmail.com>
2021-02-28 14:54:12 +0100

    Entity framework has been installed; DbContext was created, as well as properties and fluent API.

--- Park.Core/Park.cs
+++ Park.Core/Park.cs
@@ -1,12 +1,12 @@
 ﻿using System;
 using System.ComponentModel.DataAnnotations;
 
-namespace Park.Core
+namespace Parks.Core
 {
-  public class Models
+  public class Park
   {
     [Key]
-    public int Id { get; set; }
+    public Guid Id { get; set; }
     [Required]
     public string Name { get; set; }
     [Required]
--- Park.Infrastructure/DataContext/ParkDbContext.cs
+++ Park.Infrastructure/DataContext/ParkDbContext.cs
@@ -2,11 +2,34 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Parks.Core;
 
 namespace ParkAPI.DataContext
 {
   public class ParkDbContext : DbContext
   {
+    public ParkDbContext(DbContextOptions<ParkDbContext> options) : base (options)
+    {
+      
+    }
 
+    public DbSet<Park> Type { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+      modelBuilder.Entity<Park>().HasData(
+        new Park = new Park()
+        {
+          Created =  ,
+          Established = ,
+          Id = "86859558-7e97-4e84-9c5d-3065e7d3967e",
+          State = "New"
+        }
+
+
+      )
+    base.OnModelCreating(modelBuilder);
+    }
   }
 }
